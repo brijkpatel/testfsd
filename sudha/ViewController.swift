072a7fe6cdf8c851561c25cbef90cd71af15e8a8
Brijesh Patel <brijkpatel@gmail.com>
2018-09-19 14:24:45 -0600

    Initial Commit

---
+++ sudha/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  sudha
+//
+//  Created by Brijesh Patel on 2018-09-12.
+//  Copyright © 2018 Brijesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert(){
+        
+        
+        let action = UIAlertAction(title: "Awosome", style: .default, handler: nil)
+        let alert = UIAlertConsstroller(title: "hello World", message: "This is my first App", preferredStyle: .alert)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        }
+    @IBAction func sliderMoved(_slider: UIslider) {
+    print("The ")
+    }
+
+}
+
